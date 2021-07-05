370da18c6553423371a4d8318942bd722b7f065d
Adham Hamzawy <56726482+AdhamHamzawy@users.noreply.github.com>
2021-07-05 08:05:05 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -8,9 +8,8 @@
                 <div class="col-md-5">
                     <div class="about-veno">
                         <div class="logo">
-                            <img src="img/footer_logo.png" alt="Venue Logo">
+                            <img src="img1/1logo.PNG" width="150" height="150" >
                         </div>
-                        <p>Mauris sit amet quam congue, pulvinar urna et, congue diam. Suspendisse eu lorem massa. Integer sit amet posuere tellus, id efficitur leo. In hac habitasse platea dictumst.</p>
                         <ul class="social-icons">
                             <li>
                                 <a href="#"><i class="fa fa-facebook"></i></a>
@@ -25,28 +24,9 @@
                 <div class="col-md-4">
                     <div class="useful-links">
                         <div class="footer-heading">
-                            <h4>Useful Links</h4>
+                           
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Help FAQs</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Register</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Login</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>My Profile</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>How It Works?</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>More About Us</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-6">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Our Clients</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Partnerships</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Blog Entries</a></li>
-                                    <li><a href="#"><i class="fa fa-stop"></i>Contact Us</a></li>
-                                </ul>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
                 <div class="col-md-3">
@@ -56,9 +36,8 @@
                         </div>
                         <p>For more information please contact us at .</p>
                         <ul>
-                            <li><span>Phone:</span><a href="#">010000000</a></li>
-                            <li><span>Email:</span><a href="#">concrete@miu.com</a></li>
-                            <li><span>Address:</span><a href="#">MIU</a></li>
+                            <li><span>Address:</span><a href="https://www.miuegypt.edu.eg/">KM 28 Cairo – Ismailia Road Ahmed Orabi District, Cairo – Egypt</a></li>
+                            <li><span>Email:</span><a href="https://www.miuegypt.edu.eg/">miu@miuegypt.edu.eg</a></li>
                         </ul>
                     </div>
                 </div>
--- menu.php
+++ menu.php
@@ -1,6 +1,36 @@
+<?php
+ if (session_status() == PHP_SESSION_NONE)
+ {  error_reporting(E_ALL ^ E_WARNING); 
+   session_start();
+ }
+
+?>
 <html>
-<head>
-</head>
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>GPR CUT</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/datepicker.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+
+<style>
+body{
+          background-color: #F2F2F2;
+        }
+
+</style>
 <head>
   <div class="wrap">
       <header id="header">
@@ -10,7 +40,7 @@
                       <button id="primary-nav-button" type="button">Menu</button>
                       <nav id="primary-nav" class="dropdown cf">
                         <a href="index.php"><div class="logo">
-                            <img src="img/logo.png" alt="Venue Logo">
+                            <img src="img1/1logo.PNG" width="100" height="150" >
                         </div></a>
                         <ul class="dropdown menu">
                         <!-- <li class='active'><a href="userhomepage.php">Home</a></li>
@@ -20,47 +50,68 @@
                             <li class='active'><a href="services.php">Messages</a></li>
                             <li class='active'><a href="logout.php">Logout</a></li> -->
 
-                            <?php
-                            // $_SESSION["ID"]=1;
-                            // $_SESSION["Type"]="A";
-                            if(!empty($_SESSION['ID']) && $_SESSION["Type"]=="C") //client
+                            <?php  
+                               
+                               include_once("DBHelper.php");
+                          // include_once ("SginInController.php");
+                           
+                           //unserialize($_SESSION["Type"]);
+                    
+                          // unserialize($_SESSION["ID"]);
+
+                          //$_SESSION["ID"]=1; 
+                        /*  if (session_status() == PHP_SESSION_NONE)
+                          {  error_reporting(E_ALL ^ E_WARNING); 
+                            session_start();
+                          }*/
+                            require_once ("Sessions.php");
+                           // var_dump($_SESSION['ID']);
+                          //  var_dump($_SESSION['Type']);
+
+                            if(!empty($_SESSION['ID']) && $_SESSION['Type']== 1 && $_SESSION['State']== "Approved" ) //client
                             {
+                             
                               ?>
-                              <li class='active'><a href="profile.php">My profile</a></li>
+                             
                               <li class='active'><a href="user2.php">Buildings</a></li>
                               <li class='active'><a href="UserResults.php">Results</a></li>
-                              <li class='active'><a href="UserMessages.php">Messages</a></li>
+                              <li class='active'><a href="QuickTest.php">Quick Results</a></li>
+                              <li class='active'><a href="AdminMessageView.php">Messages</a></li>
+                              <li class='active'><a href="EditePasswordView.php">Edite Password</a></li>
+                              <li class='active'><a href="EditeAccView.php">Edite Acc and delete</a></li>
+
                               <li class='active'><a href="logout.php">Logout <span class="glyphicon glyphicon-log-out" ></span></a></li>
 
                               <?php
                             }
-                            else if (!empty($_SESSION['ID']) && $_SESSION["Type"]=="X") //Expert
+                            else if (!empty($_SESSION['ID']) && $_SESSION['Type']== 2 && $_SESSION['State']== "Approved" ) //Expert
                             {
                               ?>
-                              <li class='active'><a href="profile.php">My profile</a></li>
-                              <li class='active'><a href="ExpertResults.php">Results</a></li>
+                             
+                              <li class='active'><a href="QuickTest.php">Quick Test</a></li>
                               <li class='active'><a href="UserMessages.php">Messages</a></li>
+                              <li class='active'><a href="LableDataView.php">Lable Data</a></li>
                               <li class='active'><a href="logout.php">Logout <span class="glyphicon glyphicon-log-out" ></span></a></li>
                               <?php
                             }
-                            else if (!empty($_SESSION['ID']) && $_SESSION["Type"]=="A") //Admin
+                            else if (!empty($_SESSION['ID']) && $_SESSION['Type']== 3 && $_SESSION['State']== "Approved" ) //Admin
                             {
                               ?>
-                              <li class='active'><a href="profile.php">My profile</a></li>
-                              <li class='active'><a href="ViewUsers.php">Check Users</a></li>
-                              <li class='active'><a href="AddDefects.php">Defects</a></li>
-                              <li class='active'><a href="AdminMessage.php">Messages</a></li>
+                             
+                              <li class='active'><a href="ApproveRegistrationView.php">Approve Users</a></li>
+                              <li class='active'><a href="AddDefectsView.php">Defects</a></li>
+                              <li class='active'><a href="ControlUsers.php">Control the Users</a></li>
+                              <li class='active'><a href="UserMessages.php">Messages</a></li>
                               <li class='active'><a href="logout.php">Logout <span class="glyphicon glyphicon-log-out" ></span></a></li>
                               <?php
                             }
                             else
                             {
                               ?>
-                              <li class='active'><a href="signup.php">Sign Up</a></li>
-                              <li class='active'><a href="signin.php">Sing In</a></li>
+                              <li class='active'><a href="signupView.php">Sign Up</a></li>
+                              <li class='active'><a href="signinView.php">Log In</a></li>
                               <?php
                             }
-
                              ?>
 
                         </ul>
